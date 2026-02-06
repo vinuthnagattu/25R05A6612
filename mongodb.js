6dacec39de972ec10981da9ed2decb1ba53d5728
vinuthnagattu <vinuthnagattu@gmail.com>
2026-02-06 13:08:30 +0530

    Add files via upload

---
+++ mongodb.js
@@ -0,0 +1,16 @@
+const mongoose=require("mongoose")
+mongoose.connect("mongodb://localhost:27017/25R05A6612")
+.then(()=>{
+    console.log("Mongo db connected")
+})
+.catch(()=>{
+    console.log("failed")
+})
+
+const LoginSchema=new mongoose.Schema({
+    name:String,
+    password:String,
+})
+
+const collection=new mongoose.model("vinuthna",LoginSchema)
+module.exports=collection
